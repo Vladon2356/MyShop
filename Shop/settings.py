59cc5d047d1a75447b72bf98633bfaa41c373a11
Vladon2356 <kngf4427@gmail.com>
2023-07-04 18:57:49 +0300

    Add .env and .env.db files

--- Shop/settings.py
+++ Shop/settings.py
@@ -10,21 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
+from datetime import timedelta
+
 from pathlib import Path
+from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w(4q-2gnf9(a37eio_-ubrqg*hr9%@lb+&3v@nn^38edudb8f='
+SECRET_KEY = config(
+    'SECRET_KEY',
+    default="w4i=!)qh(v$f#u!22neqb2@p^*s6p^icziussmghgz)$5q5gju"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+
 
 # Application definition
 
@@ -76,16 +81,14 @@ WSGI_APPLICATION = 'Shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myshop',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': config("SQL_ENGINE", "django.db.backends.postgresql"),
+        'NAME': config("SQL_DATABASE", "postgresql"),
+        'USER': config("SQL_USER", "user"),
+        'PASSWORD': config("SQL_PASSWORD", "password"),
+        'HOST': config("SQL_HOST", "localhost"),
+        'PORT': config("SQL_PORT", "5432"),
     }
-
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
